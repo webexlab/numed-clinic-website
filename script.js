9520b2be450982861ccb2e1040e6de450c749734
ramzy mohamed <100224640+webexlab@users.noreply.github.com>
2026-06-16 20:26:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
 
-// SERVICES SLIDER
-let services = [
-    "Hair & Face Treatment",
-    "Plastic Surgery",
-    "Laser Hair Removal",
-    "Skin Treatment",
-    "Dental Services"
-];
+let isArabic = false;
 
-let sIndex = 0;
+function toggleLang() {
+    isArabic = !isArabic;
 
-function nextService() {
-    sIndex = (sIndex + 1) % services.length;
-    document.getElementById("serviceBox").innerText = services[sIndex];
-}
-
-function prevService() {
-    sIndex = (sIndex - 1 + services.length) % services.length;
-    document.getElementById("serviceBox").innerText = services[sIndex];
-}
-
-
-// BEFORE AFTER SLIDER
-let baImages = ["ba.1.png", "ba.2.png", "ba.3.png"];
-let baIndex = 0;
-
-function nextBA() {
-    baIndex = (baIndex + 1) % baImages.length;
-    document.getElementById("baImg").src = baImages[baIndex];
-}
-
-function prevBA() {
-    baIndex = (baIndex - 1 + baImages.length) % baImages.length;
-    document.getElementById("baImg").src = baImages[baIndex];
+    document.querySelectorAll("[data-en]").forEach(el => {
+        el.innerText = isArabic ? el.getAttribute("data-ar") : el.getAttribute("data-en");
+    });
 }
