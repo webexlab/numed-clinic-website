d34c14ec2fba965dfe53fca3faf53d3c3406cb72
ramzy mohamed <100224640+webexlab@users.noreply.github.com>
2026-06-16 20:23:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Nu.med Clinics website loaded successfully");
-});
+
+// SERVICES SLIDER
+let services = [
+    "Hair & Face Treatment",
+    "Plastic Surgery",
+    "Laser Hair Removal",
+    "Skin Treatment",
+    "Dental Services"
+];
+
+let sIndex = 0;
+
+function nextService() {
+    sIndex = (sIndex + 1) % services.length;
+    document.getElementById("serviceBox").innerText = services[sIndex];
+}
+
+function prevService() {
+    sIndex = (sIndex - 1 + services.length) % services.length;
+    document.getElementById("serviceBox").innerText = services[sIndex];
+}
+
+
+// BEFORE AFTER SLIDER
+let baImages = ["ba.1.png", "ba.2.png", "ba.3.png"];
+let baIndex = 0;
+
+function nextBA() {
+    baIndex = (baIndex + 1) % baImages.length;
+    document.getElementById("baImg").src = baImages[baIndex];
+}
+
+function prevBA() {
+    baIndex = (baIndex - 1 + baImages.length) % baImages.length;
+    document.getElementById("baImg").src = baImages[baIndex];
+}
